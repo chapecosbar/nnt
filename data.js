ed0248420280abffc93ae7c45a8182ee3a594d64
chapecosbar <161182372+chapecosbar@users.noreply.github.com>
2024-02-25 03:59:15 +0000

    .

--- data.js
+++ data.js
@@ -2,25 +2,25 @@ export default  [
   {
     title:"1 - Cara e Coroa",
     artist: "Nem Nome Tem",
-    cover: "1.jpeg",
+    cover: "1.jpg",
     file: "cara.mp3",
   },  
   {
     title:"2 - Cinzas",
     artist: "Nem Nome Tem",
-    cover: "1.jpeg",
+    cover: "1.jpg",
     file: "cinzas.mp3",
   },  
   {
     title:"3 - A Dança do Tchu",
     artist: "Nem Nome Tem",
-    cover: "1.jpeg",
+    cover: "1.jpg",
     file: "tchu.mp3",
   },  
   {
     title:"4 - Devastação e Reforma",
     artist: "Nem Nome Tem",
-    cover: "1.jpeg",
+    cover: "1.jpg",
     file: "devasta.mp3",
   }
  ];
