e8e755dd92261c889f60927f030051194520615b
chapecosbar <161182372+chapecosbar@users.noreply.github.com>
2024-02-25 03:50:44 +0000

    ..

--- data.js
+++ data.js
@@ -1,63 +1,27 @@
 export default  [  
   {
-    title:"1 -",
-    artist: "",
+    title:"1 - Cara e Coroa",
+    artist: "Nem Nome Tem",
     cover: "1.jpeg",
-    file: "1.mp3",
+    file: "cara.mp3",
   },  
   {
-    title:"2 -",
-    artist: "",
+    title:"2 - Cinzas",
+    artist: "Nem Nome Tem",
     cover: "1.jpeg",
-    file: "2.mp3",
+    file: "cinzas.mp3",
   },  
   {
-    title:"3 -",
-    artist: "",
+    title:"3 - A Dança do Tchu",
+    artist: "Nem Nome Tem",
     cover: "1.jpeg",
-    file: "3.mp3",
+    file: "tchu.mp3",
   },  
   {
-    title:"4 -",
-    artist: "Remix Taos Elieri",
+    title:"4 - Devastação e Reforma",
+    artist: "Nem Nome Tem",
     cover: "1.jpeg",
-    file: "4.mp3",
-  },  
-  {
-    title:"5 -",
-    artist: "",
-    cover: "1.jpeg",
-    file: "5.mp3",
-  },  
-  {
-    title:"6 -",
-    artist: "",
-    cover: "1.jpeg",
-    file: "6.mp3",
-  },  
-  {
-    title:"7 - ",
-    artist: "",
-    cover: "1.jpeg",
-    file: "7.mp3",
-  },  
-  {
-    title:"8 -,
-    artist: "Taos Elieri",
-    cover: "1.jpeg",
-    file: "8.mp3",
-  },  
-  {
-    title:"9 -",
-    artist: "",
-    cover: "1.jpeg",
-    file: "9.mp3",
-  },  
-  {
-    title:"10 -",
-    artist: "",
-    cover: "1.jpeg",
-    file: "10.mp3",
+    file: "devasta.mp3",
   }
  ];
 
